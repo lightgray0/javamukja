09896df8fa985453ea425f081ab080c076727769
soldesk <soldesk@soldesk-PC>
2019-05-31 18:21:41 +0900

    talkreply dto 생성

---
+++ javamukja/src/main/java/spring/model/talkreply/TalkReplyDTO.java
@@ -0,0 +1,41 @@
+package spring.model.talkreply;
+
+public class TalkReplyDTO {
+	private int trno;
+	private String id;
+	private int tno;
+	private String rdate;
+	private String content;
+	public int getTrno() {
+		return trno;
+	}
+	public void setTrno(int trno) {
+		this.trno = trno;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getTno() {
+		return tno;
+	}
+	public void setTno(int tno) {
+		this.tno = tno;
+	}
+	public String getRdate() {
+		return rdate;
+	}
+	public void setRdate(String rdate) {
+		this.rdate = rdate;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+
+}
---
+++ javamukja/src/main/java/spring/model/talkreply/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+/**
+ * <pre>
+ * Copyright 2019 CBD Course, Inc. All rights reserved.
+ * 패키지명        : spring.model.talkreply 
+ * 파일명          : package-info.java , 2019. 5. 31. 오후 6:12:53
+ * 작성자          : 조은서
+ * 작성자 email    : eunseo2181@naver.com	
+ * 수정내용
+ * ----------------------------------------------
+ * 수정 이력
+ * ---------------------------------------------- 
+ * 수정일      수정자  연락처
+ *   
+ * 수정내용
+ *
+ * ----------------------------------------------
+ * 
+ *</pre>
+ */
+package spring.model.talkreply;
